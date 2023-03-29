96140881d6f71e71c75f5b5537952568a0a32c14
Ian Harcombe <ian.harcombe@gmail.com>
2023-03-29 19:38:54 +0000

    Got day 18 part 2 working

--- 2017/day18b.py
+++ 2017/day18b.py
@@ -1,15 +1,12 @@
 from io import StringIO
 
-test = """set a 1
-add a 2
-mul a a
-mod a 5
-snd a
-set a 0
+test = """snd 1
+snd 2
+snd p
 rcv a
-jgz a -1
-set a 1
-jgz a -2"""
+rcv b
+rcv c
+rcv d"""
 
 def isnumber(n):
     try:
@@ -18,107 +15,127 @@ def isnumber(n):
     except ValueError:
         return False
 
-def send(x):
-    global played, recovered
+def send(pid, x):
+    q = f"q{1 if pid == 0 else 0}"
     if isnumber(x):
-        played = x
-    elif x in registers:
-        played = registers[x]
+        value = int(x)
+    elif x in registers[pid]:
+        value = registers[pid][x]
     else:
         print("Unknown register:", x)
-    # print("Played:", played)
+    registers[q].append(value)
     
-def set_register(x, y):
+def set_register(pid, x, y):
     if isnumber(y):
-        registers[x] = int(y)
-    elif y in registers:
-        registers[x] = registers[y]
+        registers[pid][x] = int(y)
+    elif y in registers[pid]:
+        registers[pid][x] = registers[pid][y]
     else:
         print("Unknown register:", y)
     
-def increase_register(x, y):
-    if x in registers:
+def increase_register(pid, x, y):
+    if x in registers[pid]:
         if isnumber(y):
-            registers[x] += int(y)
-        elif y in registers:
-            registers[x] += registers[y]
+            registers[pid][x] += int(y)
+        elif y in registers[pid]:
+            registers[pid][x] += registers[pid][y]
         else:
             print("Unknown register:", y)
     else:
         print("Unknown register:", x)
         
-def multiply_register(x, y):
-    if x in registers:
+def multiply_register(pid, x, y):
+    if x in registers[pid]:
         if isnumber(y):
-            registers[x] *= int(y)
-        elif y in registers:
-            registers[x] *= registers[y]
+            registers[pid][x] *= int(y)
+        elif y in registers[pid]:
+            registers[pid][x] *= registers[pid][y]
         else:
             print("Unknown register:", y)
     else:
         print("Unknown register:", x)
 
-def remainder_register(x, y):
-    if x in registers:
+def remainder_register(pid, x, y):
+    if x in registers[pid]:
         if isnumber(y):
-            registers[x] %= int(y)
-        elif y in registers:
-            registers[x] %= registers[y]
+            registers[pid][x] %= int(y)
+        elif y in registers[pid]:
+            registers[pid][x] %= registers[pid][y]
         else:
             print("Unknown register:", y)
     else:
         print("Unknown register:", x)
 
-def receive(x):
-    global played, recovered
-    if x in registers and registers[x] != 0:
-        recovered = played
-        print("Recovered:", recovered)
+def receive(pid, x):
+    q = f"q{pid}"
+    if len(registers[q]) > 0:
+        value = registers[q].pop(0)
+        # print("Dequeued:", value)
+        registers[pid][x] = value
+        return True
+    else:
+        return False
 
-def jump_greater(x, y):
-    if x in registers and registers[x] > 0:
+def jump_greater(pid, x, y):
+    if isnumber(x) and int(x) > 0:
         if isnumber(y):
             return int(y)
         else:
-            return registers[y]
+            return registers[pid][y]
+    elif x in registers[pid] and registers[pid][x] > 0:
+        if isnumber(y):
+            return int(y)
+        else:
+            return registers[pid][y]
     return 1
 
 commands = []
-registers = {0: {"p" : 0}, 1: {"p" : 1}}
+registers = {0: {"p" : 0}, 1: {"p" : 1}, "q0" : [], "q1" : []}
 
-with StringIO(test) as data:
-# with open("input18.txt") as data:
+# with StringIO(test) as data:
+with open("input18.txt") as data:
     for line in data:
         command, *values = line.strip().split()
         commands.append((command, values))
 
 pcs = [0, 0]
-while any([pc in range(len(commands)) for pc in pcs]):
-    for id, pc in enumerate(pcs):
+running = [True, True]
+sent = 0
+while (pcs[0] in range(len(commands)) and running[0]) or (pcs[1] in range(len(commands)) and running[1]):
+    for pid, pc in enumerate(pcs):
         if pc not in range(len(commands)):
             continue
 
-        command, values = commands[pcs]
+        assert(running or (not running and command == "rcv"))
+
+        command, values = commands[pc]
         if command == "snd":
-            send(*values)
+            send(pid, *values)
+            if pid == 1:
+                sent += 1
         elif command == "set":
-            set_register(*values)
+            set_register(pid, *values)
         elif command == "add":
-            increase_register(*values)
+            increase_register(pid, *values)
         elif command == "mul":
-            multiply_register(*values)
+            multiply_register(pid, *values)
         elif command == "mod":
-            remainder_register(*values)
+            remainder_register(pid, *values)
         elif command == "rcv":
-            receive(*values)
+            running[pid] = receive(pid, *values)
         elif command == "jgz":
-            pc += jump_greater(*values)
+            pc += jump_greater(pid, *values)
         else:
             print("Eh?", command, *values)
 
-        if command != "jgz":
+        if command != "jgz" and running[pid]:
             pc += 1
 
-        pcs[id] = pc
+        pcs[pid] = pc
+        
+        # if not all(running):
+        #    print(pcs, running, sent)
 
-print(recovered)
+print(sent)
+
+# 129 too low
