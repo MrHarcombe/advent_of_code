3bb3d9a4019127c80d68f1e72fafb6458a275e28
MrHarcombe <ian.harcombe@gmail.com>
2023-04-16 19:53:19 +0100

    Changes mostly to accommodate RedBlackTree implementation (still in progress - inserion works, deletion partly completed)

--- structures.py
+++ structures.py
@@ -1,17 +1,18 @@
+from abc import abstractmethod
+
 class TreeNode:
+    """ Simple, open, class for use in a binary search tree. Stores a given value, and
+        references to parent, left and right children. Traversal methods pass in the whole
+        node and allow access to all attributes. """
+
     def __init__(self, value):
         self.value = value
+        self.parent = None
         self.left = None
         self.right = None
 
-    def append_left(self, left):
-        self.left = left
-
-    def append_right(self, right):
-        self.right = right
-
     def pre_traverse(self, op):
-        op(self.value)
+        op(self)
         if self.left != None:
             self.left.pre_traverse(op)
         if self.right != None:
@@ -20,7 +21,7 @@ class TreeNode:
     def in_traverse(self, op):
         if self.left != None:
             self.left.in_traverse(op)
-        op(self.value)
+        op(self)
         if self.right != None:
             self.right.in_traverse(op)
 
@@ -29,40 +30,100 @@ class TreeNode:
             self.left.post_traverse(op)
         if self.right != None:
             self.right.post_traverse(op)
-        op(self.value)
+        op(self)
 
     def __str__(self):
-        return f"TreeNode({self.value}, {'NoLeft' if self.left == None else 'HasLeft'}, {'NoRight' if self.right == None else 'HasRight'})"
+        return f"TreeNode({self.value}, {'Root' if self.parent == None else 'HasParent'}, {'NoLeft' if self.left == None else 'HasLeft'}, {'NoRight' if self.right == None else 'HasRight'})"
+
+
+class RedBlackNode(TreeNode):
+    """ Extends TreeNode for use in a RedBlackTree, adds an attribute to store the colour
+        of the node; True if the node is black, False if the node is red. """
+
+    def __init__(self, value, parent):
+        super().__init__(value)
+        self.parent = parent
+        self.is_black = False
+
+    def __str__(self):
+        return f"RedBlackNode({self.value}, {'Black' if self.is_black else 'Red'}, {'Root' if self.parent == None else 'HasParent'}, {'NoLeft' if self.left == None else 'HasLeft'}, {'NoRight' if self.right == None else 'HasRight'})"
+
+    @staticmethod
+    def is_right_child(node):
+        """ Returns True if the node is a right child of its parent, False otherwise. """
+        return node.parent != None and node == node.parent.right
 
 
 class Tree:
+    """ Vanilla binary search tree implementation. Only provides checks for empty and
+        presence. Addition and standard traversal methods are also provided, no delete. """
+
     def __init__(self):
         self.root = None
 
     def is_empty(self):
         return self.root == None
 
-    def add(self, value):
-        if self.is_empty():
-            self.root = TreeNode(value)
+    def _insert(self, value : object, parent : TreeNode, on_right : bool):
+        new_node = TreeNode(value)
+
+        if parent == None:
+            self.root = new_node
+
+        elif on_right:
+            parent.right = new_node
+            new_node.parent = parent
 
         else:
-            new_node = TreeNode(value)
+            parent.left = new_node
+            new_node.parent = parent
 
+    def add(self, value : object):
+        if self.is_empty():
+            self._insert(value, None, True)
+
+        else:
             current = self.root
             looking = True
             while looking:
                 if value < current.value:
                     if current.left == None:
-                        current.append_left(new_node)
+                        self._insert(value, current, False)
                         looking = False
                     else:
                         current = current.left
 
                 else:
                     if current.right == None:
-                        current.append_right(new_node)
+                        self._insert(value, current, True)
                         looking = False
+                    else:
+                        current = current.right
+
+    @abstractmethod
+    def _delete(self, candidate : TreeNode):
+        pass
+
+    def remove(self, value : object):
+        if self.is_empty():
+            return
+        
+        else:
+            current = self.root
+            looking = True
+            while looking:
+                if value == current.value:
+                    self._delete(current)
+
+                elif value < current.value:
+                    if current.left == None:
+                        return
+                    else:
+                        current = current.left
+
+                elif value > current.value:
+                    if current.right == None:
+                        return
                     else:
                         current = current.right
 
@@ -105,6 +166,150 @@ class Tree:
     def post_traverse(self, op):
         if not self.is_empty():
             self.root.post_traverse(op)
+
+
+class RedBlackTree(Tree):
+    """ Implementation of the RedBlackTree detailed on Wikipedia as a child class of the
+        binary search tree implemented above (see https://en.wikipedia.org/wiki/Red%E2%80%93black_tree). """
+
+    def __init__(self):
+        super().__init__()
+
+    def __rotate_subtree(self, parent, direction):
+        """ Rotates the subtree (which may be the root of the tree) in the given direction
+            (True -> right, False -> left). """
+        grandparent = parent.parent
+        sibling = parent.left if direction else parent.right
+        assert (sibling != None)
+        child = sibling.right if direction else sibling.left
+
+        if direction:
+            parent.left = child
+            sibling.right = parent
+        else:
+            parent.right = child
+            sibling.left = parent
+
+        if child != None:
+            child.parent = parent
+
+        parent.parent = sibling
+        sibling.parent = grandparent
+
+        if grandparent != None:
+            if parent == grandparent.right:
+                grandparent.right = sibling
+            else:
+                grandparent.left = sibling
+        else:
+            self.root = sibling
+
+        return sibling
+
+    def _insert(self, value, parent, on_right):
+        node = RedBlackNode(value, parent)
+
+        if parent == None:
+            self.root = node
+            return
+
+        if on_right:
+            parent.right = node
+        else:
+            parent.left = node
+
+        # start of the (do while)-loop:
+        while True:
+            if parent.is_black:
+                # Case_I1 (P is black):
+                return # insertion complete
+
+            # From now on P is red.
+            grandparent = parent.parent
+            if grandparent == None:
+                # P is red and root
+                # goto Case_I4
+                parent.is_black = True
+                return
+            
+            # else: P red and G!=NULL.
+            on_right = RedBlackNode.is_right_child(parent) # the side of parent G on which node P is located
+            uncle = grandparent.left if on_right else grandparent.right
+            if (uncle == None or uncle.is_black): # considered black
+                # P is red && U is black
+                # goto Case_I56
+                if node == (parent.left if on_right else parent.right):
+                    # Case_I5 (P is red && U is black && N is inner grandchild of G):
+                    self.__rotate_subtree(parent, on_right) # P is never the root
+                    node = parent # new current node
+                    parent = grandparent.right if on_right else grandparent.left # new parent of N
+                    # fall through to Case_I6
+
+                # Case_I6 (P is red && U is black && N is outer grandchild of G):
+                self.__rotate_subtree(grandparent, not on_right) # G may be the root
+                parent.is_black = True
+                grandparent.is_black = False
+                return # insertion complete
+
+            # Case_I2 (P+U red):
+            parent.is_black = True
+            uncle.is_black = True
+            grandparent.is_black = False
+            node = grandparent # new current node
+            # iterate 1 black level higher
+            #   (= 2 tree levels)
+
+            parent = node.parent
+            if parent == None:
+                break
+        # end of the (do while)-loop
+
+    def _delete(self, candidate : RedBlackNode):
+        # need to add the simple cases first, before proceeding
+
+        parent = candidate.parent # -> parent node of N
+        on_right = RedBlackNode.is_right_child(candidate) # side of P on which N is located (∈ { LEFT, RIGHT })
+        # -> sibling of N
+        # -> close   nephew
+        # -> distant nephew
+
+        # P != NULL, since N is not the root.
+        # Replace N at its parent P by NIL:
+        P->child[dir] = NIL;
+        goto Start_D;      // jump into the loop
+
+        # start of the (do while)-loop:
+        while True:
+            dir = childDir(N);   // side of parent P on which node N is located
+            Start_D:
+            S = P->child[1-dir]; // sibling of N (has black height >= 1)
+            D = S->child[1-dir]; // distant nephew
+            C = S->child[  dir]; // close   nephew
+            if (S->color == RED)
+                goto Case_D3;                  // S red ===> P+C+D black
+            // S is black:
+            if (D != NIL && D->color == RED) // not considered black
+                goto Case_D6;                  // D red && S black
+            if (C != NIL && C->color == RED) // not considered black
+                goto Case_D5;                  // C red && S+D black
+            // Here both nephews are == NIL (first iteration) or black (later).
+            if (P->color == RED)
+                goto Case_D4;                  // P red && C+S+D black
+
+            // Case_D1 (P == NULL):
+            return; // deletion complete
+
+            // Case_D2 (P+C+S+D black):
+            S->color = RED;
+            N = P; // new current node (maybe the root)
+            // iterate 1 black level
+            //   (= 1 tree level) higher
+        
+            parent = candidate.parent
+            if parent == None:
+                break
+        # end of the (do while)-loop
+
 
 
 class ListGraph:
@@ -312,25 +517,37 @@ if __name__ == "__main__":
         t = Tree()
         for n in (7, 4, 2, 6, 16, 12, 19):
             t.add(n)
-        print(12 in t)
-        print(13 in t)
+        assert(12 in t)
+        assert(13 not in t)
         t.add(10)
-        print(10 in t)
+        assert(10 in t)
 
         tree = []
         print("Pre-order traversal:")
-        t.pre_traverse(lambda value : tree.append(str(value)))
+        t.pre_traverse(lambda n : tree.append(str(n.value)))
         print(", ".join(tree))
 
         tree = []
         print("In-order traversal:")
-        t.in_traverse(lambda value : tree.append(str(value)))
+        t.in_traverse(lambda n : tree.append(str(n.value)))
         print(", ".join(tree))
 
         tree = []
         print("Post-order traversal:")
-        t.post_traverse(lambda value : tree.append(str(value)))
+        t.post_traverse(lambda n : tree.append(str(n.value)))
         print(", ".join(tree))
+
+        print("Tree.Root:", str(t.root))
+        print("...done")
+
+    def test_red_black_tree():
+        print("Testing red-black tree...")
+        t = RedBlackTree()
+        for n in range(100):
+            t.add(n)
+
+        t.pre_traverse(lambda n: print(n.value, end=","))
+        print()
         print("...done")
 
     def test_list_graph():
@@ -446,4 +663,4 @@ if __name__ == "__main__":
             print("Connected to", e)
         print(g.matrix)
     
-    test_weighted_graph()
+    test_red_black_tree()
