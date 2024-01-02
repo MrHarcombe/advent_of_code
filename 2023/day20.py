182e80c8c36500ae709db5b8ab8018cfbbabc3d1
MrHarcombe <ian.harcombe@gmail.com>
2024-01-02 13:02:39 +0000

    Day 20 p2 - GraphViz FTW
    Goingn back, having learned from day 25, used GraphViz to illustrate the components and then ran those parts independently to figure out how many button presses each "block" would need to feed into the Conjunction module that feeds the final "rx" module... then into LCM for the final answer 👍

--- 2023/day20.py
+++ 2023/day20.py
@@ -1,5 +1,7 @@
 from collections import defaultdict
 from io import StringIO
+from math import lcm
+from time import time
 
 # test = """broadcaster -> a, b, c
 # %a -> b
@@ -16,18 +18,17 @@ test = """broadcaster -> a
 LOW = 0
 HIGH = 1
 
-
 class module:
     high_pulse_count = 0
     low_pulse_count = 0
-    button_presses = 0
-    rx_low = False
     
     def __init__(self, name, queue):
         self.name = name
         self.inputs = []
         self.outputs = []
         self.pulse_queue = queue
+        self.low_pulse_count = 0
+        self.high_pulse_count = 0
 
     def register_publisher(self, pub):
         self.inputs.append(pub)
@@ -47,19 +48,26 @@ class module:
 
     def input(self, value):
         if self.name == "rx" and value == LOW:
-            module.rx_low = Truw
+            module.rx_low = True
         # print("Unexpected input to", self.name, "LOW" if value == LOW else "HIGH")
 
+    def reset(self):
+        pass
+
     def __str__(self):
-        return f"{self.name}({type(self).__name__})"
+        return f"{self.name}_{type(self).__name__}"
 
 class broadcaster(module):
     def input(self, pulse):
-        module.button_presses += 1
         self._output(pulse[2])
 
 class button(module):
+    def __init__(self, name, queue):
+        super().__init__(name, queue)
+        self.button_presses = 0
+        
     def output(self):
+        self.button_presses += 1
         super()._output(LOW)
 
 class flipflop(module):
@@ -72,7 +80,10 @@ class flipflop(module):
         if value == LOW:
             self.state = not self.state
             super()._output(HIGH if self.state else LOW)
-            
+
+    def reset(self):
+        self.state = False
+
 class conjunction(module):
     def __init__(self, name, queue):
         super().__init__(name, queue)
@@ -81,6 +92,12 @@ class conjunction(module):
     def input(self, pulse):
         src, dest, value = pulse
         self.previous[src] = value
+        
+        if value == LOW:
+            self.low_pulse_count += 1
+        else:
+            self.high_pulse_count += 1
+        
         # print(self.name, [str(i) for i in self.inputs], self.previous)
         super()._output(LOW if all(self.previous[n.name] == HIGH for n in self.inputs) else HIGH)
         # output = LOW
@@ -88,6 +105,10 @@ class conjunction(module):
         #     if self.previous[n.name] == LOW:
         #         output = HIGH
         # super()._output(output)
+
+
+    def reset(self):
+        self.previous = defaultdict(lambda: LOW)
 
 
 pulse_queue = []
@@ -115,14 +136,59 @@ with open("input20.txt") as data:
                 modules[destination] = module(destination, pulse_queue)
             modules[name].register_listener(modules[destination.strip()])
 
+begin = time()
 start = button("start", pulse_queue)
 start.register_listener(modules["broadcaster"])
 
-while not module.rx_low:
+for _ in range(1000):
     start.output()
     while len(pulse_queue) > 0:
         pulse = pulse_queue.pop(0)
         modules[pulse[1]].input(pulse)
 
-# print("Part 1:", module.low_pulse_count * module.high_pulse_count)
-print("Part 2:", module.button_presses)
+print("Part 1:", module.low_pulse_count * module.high_pulse_count)
+print("Elapsed:", time() - begin)
+
+with open("day20-machine.dot", "w") as dot:
+    print("graph {", file=dot)
+    print("edge [colorscheme=spectral10];", file=dot)
+    print("layout=neato;", file=dot)
+    edge = 0
+
+    visited = set()
+    queue = [start]
+    while len(queue) > 0:
+        current = queue.pop(0)
+        if current not in visited:
+            visited.add(current)
+            for o in current.outputs:
+                # print(n, "--", "{", " ".join(nc for (nc, _) in connections.get_connections(n)), "}", file=dot)
+                edge += 1
+                print(current, "--", o, f'[color={edge%10}, tooltip="{current} to {o}"];', file=dot)
+                queue.append(o)
+    print("}", file=dot)
+
+print("Dot file created")
+
+parts = []
+for _ in range(4):
+    m1 = input("Top branch flip-flip: ")
+    m2 = input("Bottom branch conjunction: ")
+
+    for m in modules.values():
+        m.reset()
+    
+    begin = time()
+    start = button("start", pulse_queue)
+    start.register_listener(modules[m1])
+
+    while modules[m2].low_pulse_count == 0:
+        start.output()
+        while len(pulse_queue) > 0:
+            pulse = pulse_queue.pop(0)
+            modules[pulse[1]].input(pulse)
+
+    parts.append(start.button_presses)
+
+print("Part 2:", lcm(*parts))
+print("Elapsed:", time() - begin)
