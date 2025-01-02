eff24e944cf5862002015c920cf633687a14852b
MrHarcombe <ian.harcombe@gmail.com>
2025-01-02 10:11:42 +0000

    Reworked day 2 part 1 into functions, to accomodate inclusion of part 2 checking - starting to get more comfortable with exploring the API documentation (discovered/used slices.Concat to drop each dampened level in turn)

--- 2024.go/day2/day2.go
+++ 2024.go/day2/day2.go
@@ -20,6 +20,24 @@ var test = []string{
 
 var reports [][]int
 
+func checkReportIsSafe(report []int) bool {
+	var increasing = slices.IsSortedFunc(report, func(a, b int) int { return cmp.Compare(a, b) })
+	var decreasing = slices.IsSortedFunc(report, func(a, b int) int { return -cmp.Compare(a, b) })
+
+	if !(increasing || decreasing) {
+		return false
+	}
+
+	previous := report[0]
+	for _, value := range report[1:] {
+		if previous == value || previous-value > 3 || value-previous > 3 {
+			return false
+		}
+		previous = value
+	}
+	return true
+}
+
 func Solution(fileName string) {
 	fmt.Println("2024 Day 2")
 
@@ -52,26 +70,24 @@ func Solution(fileName string) {
 		}
 	}
 
-	var unsafe int
+	var safe_1 int
+	var safe_2 int
 
 	for _, report := range reports {
-		var increasing = slices.IsSortedFunc(report, func(a, b int) int { return cmp.Compare(a, b) })
-		var decreasing = slices.IsSortedFunc(report, func(a, b int) int { return -cmp.Compare(a, b) })
-		if increasing || decreasing {
-			previous := report[0]
-			for _, value := range report[1:] {
-				if previous == value || previous-value > 3 || value-previous > 3 {
-					// fmt.Println("Unsafe:", previous, value)
-					unsafe++
+		if checkReportIsSafe(report) {
+			safe_1++
+			safe_2++
+		} else {
+			for i := range report {
+				sub_report := slices.Concat(report[:i], report[i+1:])
+				if checkReportIsSafe(sub_report) {
+					safe_2++
 					break
 				}
-				previous = value
 			}
-		} else {
-			// fmt.Println("Unsafe:", report)
-			unsafe++
 		}
 	}
 
-	fmt.Println("Part 1:", len(reports)-unsafe)
+	fmt.Println("Part 1:", safe_1)
+	fmt.Println("Part 2:", safe_2)
 }
