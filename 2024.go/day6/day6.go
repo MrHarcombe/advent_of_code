134741160ca41aaa84ae64d847834b95dbcf5d97
MrHarcombe <ian.harcombe@gmail.com>
2025-01-11 18:59:18 +0000

    Messed around with goroutines and worker pool examples (from linked article) to try and speed up the part 2 answer for day 6

--- 2024.go/day6/day6.go
+++ 2024.go/day6/day6.go
@@ -1,5 +1,10 @@
 package day6
 
+/*
+ * worker pool solution adapted from
+ * https://codelikemachine.com/8-go-performance-tips-i-discovered-after-years-of-coding/
+ */
+
 import (
 	"fmt"
 	"strings"
@@ -22,6 +27,12 @@ type guardDetails struct {
 	col       int
 }
 
+type warehouseJob struct {
+	barrel    [2]int
+	warehouse []string
+	guard     guardDetails
+}
+
 func takeStep(grid []string, guard guardDetails) guardDetails {
 	delta := directions_delta[guard.direction]
 	next_row := guard.row + delta[0]
@@ -38,6 +49,37 @@ func takeStep(grid []string, guard guardDetails) guardDetails {
 	return guardDetails{direction: next_dir, row: next_row, col: next_col}
 }
 
+func modelGuardMovement(grid []string, initial_guard guardDetails) bool {
+	var visited = map[guardDetails]bool{initial_guard: true}
+	for guard := takeStep(grid, initial_guard); string(grid[guard.row][guard.col]) != " "; guard = takeStep(grid, guard) {
+		if visited[guard] {
+			return true
+		} else {
+			visited[guard] = true
+		}
+	}
+	return false
+}
+
+func modifyWarehouseThenModelGuard(barrel [2]int, grid []string, initialGuard guardDetails) bool {
+	var new_grid = make([]string, len(grid))
+	copy(new_grid, grid)
+	new_grid[barrel[0]] = grid[barrel[0]][:barrel[1]] + string("#") + grid[barrel[0]][barrel[1]+1:]
+	return modelGuardMovement(new_grid, initialGuard)
+}
+
+func workerPool(numWorkers int, jobs <-chan warehouseJob, results chan<- bool) {
+	for i := 0; i < numWorkers; i++ {
+		go worker(jobs, results)
+	}
+}
+
+func worker(jobs <-chan warehouseJob, results chan<- bool) {
+	for job := range jobs {
+		results <- modifyWarehouseThenModelGuard(job.barrel, job.warehouse, job.guard)
+	}
+}
+
 func Solution(rawData []string) (int, int) {
 	fmt.Println("2024 Day 6")
 	// fmt.Println(rawData)
@@ -46,12 +88,12 @@ func Solution(rawData []string) (int, int) {
 	grid[0] = strings.Repeat(" ", len(rawData[0])+2)
 	grid[len(grid)-1] = strings.Repeat(" ", len(rawData[0])+2)
 
-	var initial_guard guardDetails
+	var initialGuard guardDetails
 	for row, line := range rawData {
 		if strings.ContainsAny(line, "^>v<") {
 			for col, cell := range line {
 				if string(cell) != "." && string(cell) != "#" {
-					initial_guard = guardDetails{direction: directions_map[string(cell)], row: row + 1, col: col + 1}
+					initialGuard = guardDetails{direction: directions_map[string(cell)], row: row + 1, col: col + 1}
 					grid[row+1] = " " + rawData[row][:col] + "." + rawData[row][col+1:] + " "
 					break
 				}
@@ -62,25 +104,26 @@ func Solution(rawData []string) (int, int) {
 	}
 	// fmt.Println(guard, grid)
 
-	var visited = map[[2]int]bool{[...]int{initial_guard.row, initial_guard.col}: true}
-	for guard := takeStep(grid, initial_guard); string(grid[guard.row][guard.col]) != " "; guard = takeStep(grid, guard) {
+	var visited = map[[2]int]bool{[...]int{initialGuard.row, initialGuard.col}: true}
+	for guard := takeStep(grid, initialGuard); string(grid[guard.row][guard.col]) != " "; guard = takeStep(grid, guard) {
 		var coords = [...]int{guard.row, guard.col}
 		visited[coords] = true
 	}
 
-	var loopings int
+	jobs := make(chan warehouseJob, len(visited))
+	results := make(chan bool, len(visited))
+
+	workerPool(50, jobs, results)
+
 	for barrel := range visited {
-		var new_grid = make([]string, len(grid))
-		copy(new_grid, grid)
-		new_grid[barrel[0]] = grid[barrel[0]][:barrel[1]] + string("#") + grid[barrel[0]][barrel[1]+1:]
-		var new_visited = map[guardDetails]bool{initial_guard: true}
-		for guard := takeStep(new_grid, initial_guard); string(new_grid[guard.row][guard.col]) != " "; guard = takeStep(new_grid, guard) {
-			if new_visited[guard] {
-				loopings++
-				break
-			} else {
-				new_visited[guard] = true
-			}
+		jobs <- warehouseJob{barrel: barrel, warehouse: grid, guard: initialGuard}
+	}
+	close(jobs)
+
+	var loopings int
+	for range visited {
+		if <-results {
+			loopings++
 		}
 	}
 
