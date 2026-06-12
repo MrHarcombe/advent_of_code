193692ce392ff77d70c5d51985ccc0a07822b82b
Ian Harcombe <ian.harcombe@gmail.com>
2026-06-12 21:32:15 +0000

    Finished day 9
    With a lot of reading around (and needlessly messing with segment trees) finally got my head around compressed coordinates well enough to get to an answer for part 2.

--- 2025/day9-compressed.py
+++ 2025/day9-compressed.py
@@ -42,8 +42,7 @@ def build_borders(tiles):
     return frozenset(borders)
 
 @cache
-def is_inside(borders, point, min_x):
-
+def is_inside(borders, point, min_x=0):
     if point in borders:
         return True
     else:
@@ -61,78 +60,34 @@ def is_inside(borders, point, min_x):
 
         return hits == 1
 
-def build_rectangle_points(p1, p2):
-    px1, py1 = p1
-    px2, py2 = p2
-    
-    rectangle_points = set()
-    for y in range(min(py1, py2), max(py1, py2)+1):
-        for x in range(min(px1, px2), max(px1, px2)+1):
-            rectangle_points.add((x,y))
-            
-    return rectangle_points
+def compress_coordinates(xy, tiles, compressed_tiles):
+    return compressed_tiles[tiles.index(xy)]
 
-###
-# Non-recursive flood fill from Wikipedia
-# https://en.wikipedia.org/wiki/Flood_fill
-#
-
-def flood_fill(borders, start):
-    filled_area = set(borders)
-
-    queue = [start]
-    while len(queue):
-        fill = queue.pop(0)
-        if fill not in filled_area:
-            filled_area.add(fill)
-
-            fill_x, fill_y = fill
-            for ax, ay in ((-1,0),(1,0),(0,-1),(0,1)):
-                next_fill = (fill_x + ax, fill_y + ay)
-                if next_fill not in queue:
-                    queue.append(next_fill)
-
-    return filled_area
+def decompress_coordinates(xy, tiles, compressed_tiles):
+    return tiles[compressed_tiles.index(xy)]
 
 def part1(tiles):
     areas = {((x1,y1),(x2,y2)): (abs(x1-x2)+1) * (abs(y1-y2)+1) for ((x1,y1),(x2,y2)) in combinations(tiles,2)}
     return areas
 
-def part2(tiles, all_areas, min_x):
-    borders = build_borders(tiles)
-    # print("built borders")
-    # enclosed = flood_fill(borders, (tiles[0][0]+1, tiles[0][1]+1))
-    # print("filled enclosed shape")
+def part2(tiles, compressed):
+    borders = build_borders(compressed)
+    possible_areas = []
 
-    ignore = False
-    for (((x1,y1),(x2,y2)), area) in sorted(all_areas.items(), key=lambda item: -item[1]):
-        ## take 1, failed
-        # for x in range(min(x1, x2), max(x1, x2)+1):
-        #     for y in range(min(y1, y2), max(y1, y2)+1):
-        #         if (x,y) not in enclosed:
-        #             ignore = True
-        #             break
-        #     if ignore:
-        #         break
-        # if not ignore:
-        #     areas.append(abs(x1-x2+1) * abs(y1-y2+1))
-        # ignore = False
+    for pair in combinations(compressed, 2):
+        (x1,y1), (x2,y2) = pair
 
-        ## take 2, also failed
-        # rect_borders = build_borders([(x1,y1),(x2,y1),(x2,y2),(x1,y2)])
-        # if len(rect_borders.difference(enclosed)) == 0:
-        #     areas.append(abs(x1-x2+1) * abs(y1-y2+1))
-
-        ## take 3, using only "inside" ideas
-        # [(x1,y1),(x2,y1),(x2,y2),(x1,y2)] - do need all four corners, as all four may bridge an "outside" area
         for tile in build_borders([(x1,y1),(x2,y1),(x2,y2),(x1,y2)]):
-            if not is_inside(borders, tile, min_x):
-                print("Nope:", (x1,y1),(x2,y2))
+            if not is_inside(borders, tile):
+                # print("Nope:", (x1,y1),(x2,y2))
                 break
         else:
-            # print((x1,y1),(x2,y2),(abs(x1-x2)+1) * (abs(y1-y2)+1))
-            # biggest first, so return that
-            return area
+            tl = decompress_coordinates((x1,y1), tiles, compressed_tiles)
+            br = decompress_coordinates((x2,y2), tiles, compressed_tiles)
+            # print((x1,y1),(x2,y2), tl, br)
+            possible_areas.append(((abs(tl[0]-br[0])+1) * (abs(tl[1]-br[1])+1), tl, br))
+            
+    return max(possible_areas)
 
 tiles = []
 # with StringIO(test) as file:
@@ -143,7 +98,13 @@ with open("input9.txt") as file:
 
 unique_x = []
 unique_y = []
+compressed_tiles = []
 for (x,y) in tiles:
     if x not in unique_x: insort(unique_x, x)
     if y not in unique_y: insort(unique_y, y)
-    
+
+for (x,y) in tiles:
+    compressed_tiles.append((unique_x.index(x), unique_y.index(y)))
+
+print("Part 1:", max(part1(tiles).values()))
+print("Part 2:", part2(tiles, compressed_tiles)[0])
